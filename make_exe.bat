aa46b4cfe00bc2135462699960363b6e374bb5ff
bartush <vvictorix@gmail.com>
2023-10-10 21:39:48 +0300

    added hardcoded fish launching

--- make_exe.bat
+++ make_exe.bat
@@ -1,2 +1,2 @@
 windres msys2_fish_start.rc -O coff -o msys2_fish_start.res
-gcc -o msys2_fish_start.exe msys2_fish_start.c msys2_fish_start.res
+gcc -std=c11 -Wall -Wextra -Werror -static -municode -mwindows -o msys2_fish_start.exe msys2_fish_start.c msys2_fish_start.res
